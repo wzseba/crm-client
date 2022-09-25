037272c5c01fd03697b7b545683f0ac72df43f51
wzseba <wzseba@gmail.com>
2022-09-24 23:10:47 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+
+
+function App() {
+  
+
+  return (
+    <h1 className='text-6xl'>CRM-CLIENT</h1>
+  )
+}
+
+export default App
