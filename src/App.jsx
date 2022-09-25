634ba05b7da74c5242773d00d59eaabef3f63b93
wzseba <wzseba@gmail.com>
2022-09-25 07:33:27 -0300

    delete iniciarsecion login

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import IniciarSesion from './layout/IniciarSesion'
+
 import Layout from './layout/Layout'
 import Inicio from './paginas/Inicio'
-import LoginForm from './paginas/LoginForm'
+import NuevoCliente from './paginas/NuevoCliente'
+import EditarCliente from './paginas/EditarCliente'
 
 
 
@@ -13,12 +14,10 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<IniciarSesion/>}>
-          <Route index element={<LoginForm/>}/>
-        </Route>
-
         <Route path='/clientes' element={<Layout/>}>
           <Route index element={<Inicio />}/>
+          <Route path='nuevo' element={<NuevoCliente/>}/>
+          <Route path='editar/:id' element={<EditarCliente/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
