becb475a8782a555b3f79f6f7b3fa62adb0da5d9
wzseba <wzseba@gmail.com>
2022-09-25 11:04:09 -0300

    update

--- src/paginas/NuevoCliente.jsx
+++ src/paginas/NuevoCliente.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import Formulario from "../components/Formulario"
+
 
 const Nuevo = () => {
   return (
-    <div>Nuevo</div>
+    <>
+      <h1 className='font-black text-4xl text-blue-800'>Nuevo Cliente</h1>
+      <p className='mt-3 font-bold text-gray-600'> Llena los siguentes campos para registrar un cliente</p>
+
+      <Formulario/>
+    </>
   )
 }
 
