eefcf4f3fef61bab0c2c5cd426893867acdb8d91
wzseba <wzseba@gmail.com>
2022-10-02 22:33:18 -0300

    actualizaciones varaias

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -7,10 +7,10 @@ const Layout = () => {
 
   return (
     <div className="md:flex md:min-h-screen" >
-        <div className="md:w-1/4 bg-blue-800 px-5 py-10">
+        <aside className="md:w-1/4 bg-blue-800 px-5 py-10">
           <h2 className="text-4xl font-black text-center text-white">CRM Clientes</h2>
             <nav className="mt-10">
-              <Link to="/clientes"
+              <Link to="/"
                 className={`${urlActual === '/clientes' ? 'text-blue-300' : 'text-white' } text-2xl block mt-2 hover:text-blue-300`}
               >
                 Clientes
@@ -22,7 +22,7 @@ const Layout = () => {
                 Nuevo Cliente
               </Link>
             </nav>
-        </div>
+        </aside>
 
         <div className="md:w-3/4 p-10 bg-gray-100 md:h-screen overflow-scroll">
           <Outlet/>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { Children } from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
+import Layout from './layout/Layout'
+import NuevoCliente from './paginas/NuevoCliente'
+import Inicio, { loader as clienteLoader } from './paginas/Inicio'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Inicio />,
+        loader: clienteLoader
+      },
+      {
+        path: '/clientes/nuevo',
+        element: <NuevoCliente />
+      }
+    ]
+  },
+  {
+
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 )
