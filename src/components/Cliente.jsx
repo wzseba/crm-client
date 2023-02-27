3bb725bf0eb0b021fcce64e2cc5e54554192f46f
wzseba <wzseba@gmail.com>
2023-02-26 21:44:25 -0300

    se agrego funcionalidad a los botones editar y eliminar

--- src/components/Cliente.jsx
+++ src/components/Cliente.jsx
@@ -1,7 +1,16 @@
+import { Form, redirect, useNavigate } from "react-router-dom";
+import { eliminarCliente } from "../data/Clientes";
+
+export async function action({params}){
+    await eliminarCliente(params.clienteId);
+    return redirect('/');
+}
 
 
 const Cliente = ({cliente}) => {
 
+    const navigate = useNavigate();
+    
     const { nombre, empresa, email, telefono, id} = cliente
 
   return (
@@ -18,15 +27,27 @@ const Cliente = ({cliente}) => {
             <button
                 type="button"
                 className="text-blue-600 hover:text-blue-700 uppercase font-bold text-xs"
+                onClick={()=> navigate(`/clientes/${id}/editar`)}
             >
                 Editar
             </button>
-            <button
-                type="button"
+            <Form
+                method="post"
+                action={`/clientes/${id}/eliminar`}
+                onSubmit={(e)=>{
+                    if(!confirm('Desea elimnar este registro?')){
+                        e.preventDefault();
+                    }
+                }}
+            >
+                <button
+                type="submit"
                 className="text-red-600 hover:text-red-700 uppercase font-bold text-xs"
             >
                 Eliminar
             </button>
+            </Form>
+            
         </td>
     </tr>
   )
